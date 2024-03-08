ce3d9d527d096bc9ce3163277d1df3249b4261ac
TobiaRigon <rigontobia@gmail.com>
2024-03-08 22:10:42 +0100

    Footer

--- src/components/FooterComponent.vue
+++ src/components/FooterComponent.vue
@@ -1,5 +1,22 @@
 <template>
-  <div>
-    <h1>Footer</h1>
+  <div class="container">
+    <div>
+      <h5>Contact</h5>
+    </div>
+    <div>
+      <a href="https://www.linkedin.com/in/tobia-rigon-ba0b051a2/">Linkedin</a>
+      <a href="https://github.com/TobiaRigon">GitHub</a>
+      <a href="mailto:rigontobia@gmail.com"> Email </a>
+    </div>
   </div>
 </template>
+
+<style scoped lang="scss">
+a {
+  text-decoration: none;
+  color: black;
+  margin-right: 8px;
+  font-size: 18px;
+  font-weight: 500;
+}
+</style>
