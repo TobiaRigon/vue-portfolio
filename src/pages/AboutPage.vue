98fa1b6830ecb1d6fb489fc8f6c2b404e0b8866e
TobiaRigon <rigontobia@gmail.com>
2024-03-10 15:58:42 +0100

    About: content

--- src/pages/AboutPage.vue
+++ src/pages/AboutPage.vue
@@ -1,6 +1,48 @@
 <template>
   <div>
-    <h1>About</h1>
+    <div class="container d-flex justify-content-between">
+      <div class="col-7 mx-5">
+        <h1>Hey there!</h1>
+        <p>
+          I'm Tobia, a passionate 3D enthusiast with a solid background in CGI.
+          Based in Italy, I've recently embarked on an exciting journey into the
+          world of web development. Through this site, you can join me as I
+          navigate this new terrain and document my progress. As a former CGI
+          artist, I've honed my skills in crafting immersive digital
+          experiences, and now, as a junior web developer, I'm eager to
+          translate that creativity into the realm of web design and
+          development. My fascination with shapes, textures, and storytelling
+          fuels my quest to master the art of coding and design in the digital
+          landscape. When I'm not coding or experimenting with new technologies,
+          you can find me exploring the outdoors or indulging in my love for
+          gaming. From hiking trails to virtual worlds, I find inspiration in
+          every corner of life. Thanks for stopping by my portfolio! Let's
+          embark on this journey together as we explore the endless
+          possibilities of web development and design.
+        </p>
+      </div>
+      <div class="col-2 mx-5">
+        <div>
+          <h5>What i Like</h5>
+          <ul>
+            <li>
+              <a href="https://open.spotify.com/user/11153954997/playlists"
+                >Making Playlists</a
+              >
+            </li>
+            <li>Auto UV</li>
+          </ul>
+        </div>
+
+        <div>
+          <h5>What i'm Lerning</h5>
+          <ul>
+            <li>Three.js</li>
+            <li>Auto UV</li>
+          </ul>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
