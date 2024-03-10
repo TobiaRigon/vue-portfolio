e0b85910cc321a8662426a28c4acbd0b50c02681
TobiaRigon <rigontobia@gmail.com>
2024-03-10 18:56:05 +0100

    Work: Jumbo

--- src/js/threeWork.js
+++ src/js/threeWork.js
@@ -2,125 +2,58 @@ import * as THREE from "three";
 import gsap from 'gsap';
 
 export function setupThree(canvas) {
-  // scene
+  // Scene
   const scene = new THREE.Scene();
 
+  // Array per memorizzare gli oggetti mesh
+  const meshes = [];
 
   // scroll
   const transformGeo = [
+
     {
-      rotationZ: 0.45,
-      positionX: 1.5,
-      heightSegments: 3 // Numero di segmenti in altezza per la prima sezione
-    },
-    {
-      rotationZ: -2,
-      positionX: -1.7,
       heightSegments: 4 // Numero di segmenti in altezza per la seconda sezione
     },
     {
-      rotationZ: -3.5,
-      positionX: 0,
       heightSegments: 80 // Numero di segmenti in altezza per la terza sezione
     },
+    {
+      heightSegments: 3 // Numero di segmenti in altezza per la prima sezione
+    },
+  ];
 
-  ]
-
-  // test cube
-    // Altezza dei segmenti iniziale
-    let heightSegments = transformGeo[0].heightSegments;
-
+  // Test cube
+  let heightSegments = 3;
   const cylinderGeometry = new THREE.CylinderGeometry(1.5, 1.5, 1.5, heightSegments);
-
-  const colors = ['#db3535', '#4ec439', '#39b2c4', '#dbcb35']; // Array di colori
-  let currentColorIndex  = 0;// Colore iniziale
+  const colors = ['#db3535', '#4ec439', '#39b2c4']; // Array di colori
+  let currentColorIndex  = 0; // Colore iniziale
   const mainMaterial = new THREE.MeshBasicMaterial({
     color: parseInt(colors[currentColorIndex].replace(/^#/, ''), 16),
   });
   const mainGeo = new THREE.Mesh(cylinderGeometry, mainMaterial);
+  mainGeo.position.x = 0;
+  mainGeo.rotation.x = Math.PI * 0.15;
+  mainGeo.rotation.z = Math.PI * 0.25;
 
-  mainGeo.position.x = 1.5 
-  mainGeo.rotation.x = Math.PI * 0.2 
-  mainGeo.rotation.z = Math.PI * 0.15
-
-  console.log(mainGeo);
   scene.add(mainGeo);
+  meshes.push(mainGeo); // Aggiungi mainGeo all'array meshes
 
-  // scroll
+  // Aggiungi un event listener al click dell'oggetto mainGeo
+  mainGeo.addEventListener('click', () => {
+    console.log('Click su mainGeo!');
+    // Cambia colore dell'oggetto
+    currentColorIndex = (currentColorIndex + 1) % colors.length;
+    mainGeo.material.color.set(parseInt(colors[currentColorIndex].replace(/^#/, ''), 16));
   
+  });
 
-  let scrollY = window.scrollY
-  let currentSection = 0
-  
-  window.addEventListener('scroll', () =>{
-    scrollY = window.scrollY
-    const newSection = Math.round(scrollY / sizes.height)
-    console.log(newSection)
-
-    if (newSection != currentSection) {
-      currentSection = newSection
-
-        // Calcola la posizione della sezione corrente
-    const sectionPosition = currentSection * sizes.height;
-
-    // Snappa lo scroll alla posizione della sezione
-    // window.scrollTo({
-    //   top: sectionPosition,
-    //   behavior: 'smooth' // Opzionale: aggiunge un'animazione di scorrimento fluida
-    // });
-    
-
-   
-
-      if (!!mainGeo) {
-          gsap.to(
-            mainGeo.rotation, {
-                  duration: 1.5,
-                  ease: 'power2.inOut',
-                  z: transformGeo[currentSection].rotationZ
-              }
-          )
-          gsap.to(
-            mainGeo.position, {
-                  duration: 1.5,
-                  ease: 'power2.inOut',
-                  x: transformGeo[currentSection].positionX
-              }
-          )
-
-
-          // Cambia il colore dell'oggetto
-          currentColorIndex = currentSection % colors.length; // Calcola l'indice del colore
-          mainGeo.material.color.set(colors[currentColorIndex]); // Imposta il colore
-
-
-           // Aggiorna il numero di segmenti in altezza
-        heightSegments = transformGeo[currentSection].heightSegments;
-        mainGeo.geometry.dispose(); // Rimuove la geometria corrente
-        mainGeo.geometry = new THREE.CylinderGeometry(1.5, 1.5, 1.5, heightSegments); // Crea nuova geometria con il nuovo numero di segmenti
-
-
-
-          // gsap.to(
-          //     sphereShadow.position, {
-          //         duration: 1.5,
-          //         ease: 'power2.inOut',
-          //         x: transformGeo[currentSection].positionX - 0.2
-          //     }
-          // )
-      }
-  }
-  })
-
-
-
-  // size
+  // Size
   const sizes = {
     width: window.innerWidth,
     height: window.innerHeight,
   };
 
-  // camera
+  // Camera
   const camera = new THREE.PerspectiveCamera(
     35,
     sizes.width / sizes.height,
@@ -130,49 +63,69 @@ export function setupThree(canvas) {
   camera.position.z = 8;
   scene.add(camera);
 
-
-
-  // renderer
+  // Renderer
   const renderer = new THREE.WebGLRenderer({
     canvas: canvas,
     antialiasing: true,
     alpha: true,
   });
-
   renderer.setSize(sizes.width, sizes.height);
   renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
-
   renderer.render(scene, camera);
 
-// animate
+  // Raycaster
+  const raycaster = new THREE.Raycaster();
+  const mouse = new THREE.Vector2();
 
-const clock = new THREE.Clock()
-let lastElapsedTime = 0
+  // Funzione per gestire il clic del mouse
+  function onMouseClick(event) {
+    // Calcola la posizione del clic del mouse nella finestra
+    mouse.x = (event.clientX / sizes.width) * 2 - 1;
+    mouse.y = -(event.clientY / sizes.height) * 2 + 1;
 
-const tick = () => {
-    const elapsedTime = clock.getElapsedTime()
-    const deltaTime = elapsedTime - lastElapsedTime
-    lastElapsedTime = elapsedTime
+    // Effettua il raycasting
+    raycaster.setFromCamera(mouse, camera);
+    const intersects = raycaster.intersectObjects(meshes);
 
-     // Calcola l'angolo di rotazione basato sul tempo trascorso
-     const rotationSpeed = 0.5; // Velocità di rotazione
-     const rotationAngle = elapsedTime * rotationSpeed;
- 
-     // Imposta l'angolo di rotazione lungo l'asse y
-     mainGeo.rotation.y = rotationAngle;
+    // Se ci sono intersezioni, gestisci il clic
+    if (intersects.length > 0) {
+      console.log('Clic su un oggetto!');
+      // Cambia colore dell'oggetto
+      currentColorIndex = (currentColorIndex + 1) % colors.length;
+      mainGeo.material.color.set(parseInt(colors[currentColorIndex].replace(/^#/, ''), 16));
+    
 
-    // respiro
-    if(!!mainGeo){
-      mainGeo.position.y = Math.sin(elapsedTime*0.5) * 0.15 -0.15
+      // Cambia rotationZ in base alla configurazione in transformGeo
+  const nextTransform = transformGeo.shift(); // Prendi il prossimo set di trasformazioni
+  transformGeo.push(nextTransform); // Metti il set di trasformazioni al fondo dell'array
+  heightSegments = nextTransform.heightSegments;
+  mainGeo.geometry.dispose();
+  mainGeo.geometry = new THREE.CylinderGeometry(1.5, 1.5, 1.5, heightSegments);
+  mainGeo.geometry.computeVertexNormals(); // Calcola le normali dei vertici per la geometria del cilindro
+
+    
     }
+  }
+
+  // Aggiungi un event listener per il clic del mouse
+  window.addEventListener('click', onMouseClick);
+
+  // Animate
+  const clock = new THREE.Clock();
+  const tick = () => {
+    const elapsedTime = clock.getElapsedTime();
+    const rotationSpeed = 0.5; // Velocità di rotazione
+    const rotationAngle = elapsedTime * rotationSpeed;
+    mainGeo.rotation.y = rotationAngle;
+
+    // Respiro
+    mainGeo.position.y = Math.sin(elapsedTime * 0.5) * 0.15 - 0.15;
 
     // Render
-    // console.log('tick')
-    renderer.render(scene, camera)
+    renderer.render(scene, camera);
 
-    // Call tick again on the next frame
-    window.requestAnimationFrame(tick)
-}
-tick(); 
-
+    // Richiedi il prossimo frame di animazione
+    window.requestAnimationFrame(tick);
+  };
+  tick();
 }
